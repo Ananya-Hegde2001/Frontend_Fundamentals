0e38c4699545700c2e859d411f8885bc2a52cae3
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-11-08 10:01:32 +0530

    1 pgm

---
+++ JavaScript_Essentials/Promise All Polyfill.js
@@ -0,0 +1,17 @@
+function myPromiseAll(promises) {
+  return new Promise((resolve, reject) => {
+    let results = [];
+    let completed = 0;
+
+    promises.forEach((p, i) => {
+      Promise.resolve(p).then(value => {
+        results[i] = value;
+        completed++;
+        if (completed === promises.length) resolve(results);
+      }).catch(reject);
+    });
+  });
+}
+
+myPromiseAll([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)])
+  .then(console.log);
