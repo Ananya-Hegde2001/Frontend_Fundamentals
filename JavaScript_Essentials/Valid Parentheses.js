16cbec103a008def39dc353fbabff2bb42f3768c
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-11-17 08:35:33 +0530

    3

---
+++ JavaScript_Essentials/Valid Parentheses.js
@@ -0,0 +1,13 @@
+function isValid(s) {
+  let stack = [];
+  let map = { ")": "(", "}": "{", "]": "[" };
+
+  for (let ch of s) {
+    if (ch in map) {
+      if (stack.pop() !== map[ch]) return false;
+    } else stack.push(ch);
+  }
+  return stack.length === 0;
+}
+
+console.log(isValid("(){}[]"));
