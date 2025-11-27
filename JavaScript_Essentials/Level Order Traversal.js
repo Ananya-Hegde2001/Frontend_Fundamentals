61622b16e023c3e10b93f03b0ef878db28048dc3
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-11-27 15:51:14 +0530

    2 pgm

---
+++ JavaScript_Essentials/Level Order Traversal.js
@@ -0,0 +1,22 @@
+var levelOrder = function(root) {
+    if (!root) return [];
+    let queue = [root];
+    let result = [];
+
+    while (queue.length) {
+        let size = queue.length;
+        let level = [];
+
+        for (let i = 0; i < size; i++) {
+            let node = queue.shift();
+            level.push(node.val);
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        result.push(level);
+    }
+
+    return result;
+};
