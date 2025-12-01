c751a37f042b9f74c5590576ee3e41f3315e23c6
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-12-01 16:38:40 +0530

    1 pgm

---
+++ JavaScript_Essentials/Merge Sorted Array.js
@@ -0,0 +1,8 @@
+var merge = function(nums1, m, nums2, n) {
+    let i = m - 1, j = n - 1, k = m + n - 1;
+
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+        else nums1[k--] = nums2[j--];
+    }
+};
