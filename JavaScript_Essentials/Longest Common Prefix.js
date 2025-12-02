db7be1d9277ea9bb3369700225619f6f5d36bd4f
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-12-02 09:34:56 +0530

    1 pgm

---
+++ JavaScript_Essentials/Longest Common Prefix.js
@@ -0,0 +1,10 @@
+var longestCommonPrefix = function(strs) {
+    let prefix = strs[0];
+
+    for (let str of strs) {
+        while (!str.startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
+        }
+    }
+    return prefix;
+};
