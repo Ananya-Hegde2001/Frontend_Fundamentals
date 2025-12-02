ac50d27c864ff7b1ed60cff797d3ef3a16dfe45a
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-12-02 09:35:09 +0530

    3 pgm

---
+++ JavaScript_Essentials/Validate Anagram.js
@@ -0,0 +1,11 @@
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    let map = {};
+
+    for (let c of s) map[c] = (map[c] || 0) + 1;
+    for (let c of t) {
+        if (!map[c]) return false;
+        map[c]--;
+    }
+    return true;
+};
