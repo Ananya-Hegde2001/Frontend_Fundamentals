d09f67e13a450af62303f00000c00481db00e203
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-12-08 23:00:45 +0530

    2 pgm

---
+++ JavaScript_Essentials/Count Vowels.js
@@ -0,0 +1,11 @@
+function countVowels(str) {
+  let vowels = "aeiouAEIOU";
+  let count = 0;
+
+  for (let ch of str) {
+    if (vowels.includes(ch)) count++;
+  }
+  return count;
+}
+
+console.log(countVowels("Javascript"));
