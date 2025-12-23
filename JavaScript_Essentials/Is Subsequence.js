bbcf9dcf661fc285c9d206714581fe82af24776d
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-12-23 12:46:25 +0530

    2 pgm

---
+++ JavaScript_Essentials/Is Subsequence.js
@@ -0,0 +1,18 @@
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+    }
+    j++;
+  }
+  
+  return i === s.length;
+}
+
+console.log("\n=== BONUS Program 5: Is Subsequence ===");
+console.log(isSubsequence("abc", "ahbgdc")); 
+console.log(isSubsequence("axc", "ahbgdc")); 
+console.log(isSubsequence("ace", "abcde")); 
