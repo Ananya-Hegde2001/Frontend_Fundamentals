587ced77d80841b7aaa4cb941f3f3affdf138a81
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-12-28 11:21:52 +0530

    2 pgm

---
+++ JavaScript_Essentials/Convert String to Array of Characters.js
@@ -0,0 +1,8 @@
+function stringToCharArray(str) {
+  return [...str];
+}
+
+console.log("\n=== BONUS Program 8: String to Char Array ===");
+console.log(stringToCharArray("hello"));
+console.log(stringToCharArray("123"));
+console.log(stringToCharArray("a b c"));
