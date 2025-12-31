e0a686941675c53e23c233be5322e907e90fb3c5
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-12-31 15:52:45 +0530

    2 pgm

---
+++ JavaScript_Essentials/Repeat String N Times.js
@@ -0,0 +1,8 @@
+function repeatString(str, times) {
+  return str.repeat(times);
+}
+
+console.log("\n=== BONUS Program 10: Repeat String ===");
+console.log(repeatString("abc", 3)); 
+console.log(repeatString("hello", 2));
+console.log(repeatString("*", 5));
