49ae9d6e3483558a91b1959effc820333ba2aa77
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2026-01-22 17:34:36 +0530

    3 pgm

---
+++ JavaScript_Essentials/Smart Quote Converter.js
@@ -0,0 +1,16 @@
+function smartQuotes(text) {
+  text = text.replace(/"([^"]*)"/g, '"$1"');
+  
+  text = text.replace(/(\w)'(\w)/g, '$1'$2'); // Apostrophes
+  text = text.replace(/'([^']*)'/g, ''$1''); // Single quotes
+  
+  text = text.replace(/\b"(\w)/g, '"$1');
+  text = text.replace(/(\w)"\b/g, '$1"');
+  
+  return text;
+}
+
+console.log("\n=== Program 2: Smart Quote Converter ===");
+console.log(smartQuotes(`He said "Hello" and that's all.`));
+console.log(smartQuotes(`"Don't worry," she said.`));
+console.log(smartQuotes(`It's a "beautiful" day.`));
