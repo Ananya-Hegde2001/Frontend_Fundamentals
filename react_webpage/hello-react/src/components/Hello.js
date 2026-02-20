02a9173cb3242b3f22fd6e5b97833428346491d2
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2026-02-20 23:00:58 +0530

    update

--- react_webpage/hello-react/src/components/Hello.js
+++ react_webpage/hello-react/src/components/Hello.js
@@ -4,6 +4,7 @@ function Hello() {
   return (
     <div>
       <h1>Hello World</h1>
+      <pre>I am Ananya V Hegde and i can able to dance and sing in the same time</pre>
       <p>My first React page</p>
     </div>
   );
